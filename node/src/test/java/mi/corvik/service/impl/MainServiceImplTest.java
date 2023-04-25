96717926addcbd0a5b516c7255b673aa6cebb578
mikor85 <cormakoff@gmail.com>
2023-04-25 21:30:22 +0300

    Added some changes.

---
+++ node/src/test/java/mi/corvik/service/impl/MainServiceImplTest.java
@@ -0,0 +1,38 @@
+package mi.corvik.service.impl;
+
+import mi.corvik.dao.RawDataDAO;
+import mi.corvik.entity.RawData;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootTest
+public class MainServiceImplTest {
+
+    @Autowired
+    private RawDataDAO rawDataDAO;
+
+    @Test
+    public void testSaveRawData() {
+        Update update = new Update();
+        Message message = new Message();
+        message.setText("ololo");
+        update.setMessage(message);
+
+        RawData rawData = RawData.builder()
+                .event(update)
+                .build();
+        Set<RawData> testData = new HashSet<>();
+
+        testData.add(rawData);
+        rawDataDAO.save(rawData);
+
+        Assert.isTrue(testData.contains(rawData), "Entity not found in Set");
+    }
+}
